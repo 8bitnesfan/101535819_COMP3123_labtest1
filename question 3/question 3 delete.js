2716822f39d7bdd8b70861b8e10cdd5e51e92183
Aidan Repchik <aidan.repchik@gmail.com>
2025-10-02 19:46:51 -0400

    question 3 part 2

--- question 3/question 3 delete.js
+++ question 3/question 3 delete.js
@@ -6,20 +6,20 @@ const path = require('path');
 const logsDir = path.join(process.cwd(), 'Logs');
 
 const deleteLogs = dir => {
-    if (!fs.existsSync(dir)) {
-        console.log('Logs directory does not exist');
-        return;
+    if (fs.existsSync(dir)) {
+        fs.readdirSync(dir).forEach(file => {
+            const filePath = path.join(dir, file);
+            if (fs.lstatSync(filePath).isFile()) {
+                fs.unlinkSync(filePath);
+                console.log(`Deleted file: ${file}`);
+            }
+        });
+
+        fs.rmdirSync(dir);
+        console.log('Logs directory removed.');
+    } else {
+        console.log('Logs directory does not exist.');
     }
-
-    fs.readdirSync(dir).forEach(file => {
-        const filePath = path.join(dir, file);
-        if (fs.lstatSync(filePath).isFile()) {
-            fs.unlinkSync(filePath);
-            console.log(`delete files...${file}`);
-        }
-    });
-
-    fs.rmdirSync(dir);
 };
 
 deleteLogs(logsDir);
